f4b8737e3d3070667388b07a53df47b0d3e7afc0
Shereal McNeal <opcommander1@Shereal-McNeals-MacBook-Pro.local>
2019-03-21 12:36:28 -0500

    test env

--- src/js/models/Credits.js
+++ src/js/models/Credits.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // import {key, proxy } from '../config';
-import {S3} from '../config';
+import {s3} from '../config';
 
   //Search movie api by id and returns actors credits
   export default class Credits {
@@ -10,7 +10,7 @@ import {S3} from '../config';
 
     async getCreditResults() {
       try {
-        const res = await axios(`${S3.PROXY}https://api.themoviedb.org/3/movie/${this.id}/credits?api_key=${S3.KEY}`);
+        const res = await axios(`${s3.PROXY}https://api.themoviedb.org/3/movie/${this.id}/credits?api_key=${s3.KEY}`);
         this.result = res.data.cast;
       } catch (error) {
         alert(error);

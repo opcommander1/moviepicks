adb013d894721f8563b288281f425a5b9c187f47
Shereal McNeal <opcommander1@Shereal-McNeals-MacBook-Pro.local>
2019-03-21 11:34:09 -0500

    added port setup

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
         filename: 'js/bundle.js',
     },
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        port: 8888
     },
     plugins: [
         new HtmlWebpackPlugin({

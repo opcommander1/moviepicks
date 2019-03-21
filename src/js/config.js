446d2e55775210a6b703ca7483d68d01fed19204
Shereal McNeal <opcommander1@Shereal-McNeals-MacBook-Pro.local>
2019-03-21 12:41:28 -0500

    test env

--- src/js/config.js
+++ src/js/config.js
@@ -1,9 +1,9 @@
 // export const proxy = 'https://cors-anywhere.herokuapp.com/';
 // export const key = '3d6ac87c32c71a4673657cec3692d846';
 
-import aws from 'aws';
 
-export let s3 = new aws.S3({
+
+export let s3 = {
   KEY: process.env.KEY,
   PROXY: process.env.PROXY
-});
+};

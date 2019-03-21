5ee3e55d1edbf7cbceecb1acb090601b4aa429c2
Shereal McNeal <opcommander1@hotmail.com>
2019-03-21 13:51:37 -0500

    test env

--- src/js/models/Credits.js
+++ src/js/models/Credits.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+require('dotenv').config();
 // import {key, proxy } from '../config';
 
   //Search movie api by id and returns actors credits
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 
 
 
-
 export default class Search {
   constructor(query) {
     this.query = query;

23c3605bf30f85a60e1837b1ebda4a9db1d93b94
Shereal McNeal <opcommander1@hotmail.com>
2019-03-21 15:38:23 -0500

    test env

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // import {key, proxy } from '../config';
-require('dotenv').config();
+var KEY = process.env.KEY;
+var PROXY = process.env.PROXY;
 
 
 
@@ -12,7 +13,7 @@ export default class Search {
   //Finds the requested api movie
   async getMovieResults() {
       try {
-        const res = await axios(`${process.env.PROXY}https://api.themoviedb.org/3/search/movie?api_key=${process.env.KEY}&query=${this.query}`);
+        const res = await axios(`${PROXY}https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=${this.query}`);
         this.result = res.data.results;
       } catch (error) {
         alert(error);

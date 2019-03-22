f42ba495a32bd2b63881e61cb122b6fefa934a94
Shereal McNeal <opcommander1@hotmail.com>
2019-03-21 20:40:42 -0500

    test env

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
-export const PROXY = 'https://cors-anywhere.herokuapp.com/';
-export const KEY = '3d6ac87c32c71a4673657cec3692d846';
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '3d6ac87c32c71a4673657cec3692d846';
 

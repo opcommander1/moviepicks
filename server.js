bc034358c54b228c509af3b3a9e75d02052d4f6d
Shereal McNeal <opcommander1@hotmail.com>
2019-03-21 20:32:33 -0500

    test env

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const {KEY} = require('./src/js/config');
+require('dotenv').config();
 
-console.log(`Hi my name is ${KEY}`);
+console.log(`Hi my name is ${process.env.MOVIE_KEY}`);
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + '/dist'));
---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+export const PROXY = 'https://cors-anywhere.herokuapp.com/';
+export const KEY = '3d6ac87c32c71a4673657cec3692d846';
+

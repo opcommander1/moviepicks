c1e64715d117c361eebb3a4abe2572de73871708
Shereal McNeal <opcommander1@hotmail.com>
2019-03-21 19:42:23 -0500

    test env

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-require('dotenv').config();
+const {KEY} = require('./src/js/config');
 
-console.log(`Hi my name is ${process.env.KEY}`);
+console.log(`Hi my name is ${KEY}`);
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + '/dist'));
